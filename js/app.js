038163aa230b6a2610a8f2bc5d0f1b45e4254db2
Asa Toburen <atopettit@gmail.com>
2014-11-21 15:02:49 -0700

    refactored with functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 $(document).ready( function (){
 
-for (var x= 1; x <= 100; x++){
+var number = prompt("Please enter a number::");
+
+//converts strings to numbers
+
+var integer = +number;
+
+//checks to see if prompt result is a whole number.
+
+if (integer % 1 != 0) {
+
+	 integer =prompt("Please enter a number!!!") }; 
+
+//counts from 1 to the number submitted to the prompt with FizzBuzzes
+
+var counts = function(integer){
+
+ for (var x= 1; x <= integer; x++){
 	if ((x % 3 == 0) && (x % 5 == 0)) {
 		document.write('FizzBuzz');
 	} else if ( x % 3 == 0) {
@@ -8,8 +24,15 @@ for (var x= 1; x <= 100; x++){
 	} else if ( x % 5 == 0) {
 		document.write('Buzz');
 	}	else {document.write(" "+x+" ");
-}
+
+};
+};
+
+//appends text to div  in html
 
 $('#output').append('x');
 };
+
+counts(integer);
+
 });
